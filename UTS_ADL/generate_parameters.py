1d458bf3efd94c8ed09e3b14b79168a91ee02811
Thyeadi Tungson <99853830+thyeaditungson29@users.noreply.github.com>
2022-04-23 08:01:17 +0700

    Update generate_parameters.py

--- UTS_ADL/generate_parameters.py
+++ UTS_ADL/generate_parameters.py
@@ -7,7 +7,7 @@ params_name = "parameters.txt"
 save_folder = "Model0"
 
 
-model = "Architectures/DoubleLinked.py"
+model = "/content/mystudy_ADL2022/UTS_ADL/XNet.py"
 name = "DLs200_64"
 
 lrate = 0.0001
@@ -19,8 +19,8 @@ filter_list =  [64,128,256, 512, 1024]
 loss = "categorical_crossentropy"
 #loss = "jaccard"
 #data = "Humans_CT_Phantom_s224.hdf5"
-data = "cv_dataset_s200.hdf5"
-no_epochs = 5000
+data = "/content/mystudy_ADL2022/UTS_ADL"
+no_epochs = 10
 duplicate = True
 
 
